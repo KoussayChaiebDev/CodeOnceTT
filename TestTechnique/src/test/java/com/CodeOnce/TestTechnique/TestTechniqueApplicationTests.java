9b5156fe57cf79d74b77ddc54e919bafd1882b84
KoussayChaiebProf <112632164+KoussayChaiebProf@users.noreply.github.com>
2022-12-03 13:27:52 +0100

    Add files via upload

---
+++ TestTechnique/src/test/java/com/CodeOnce/TestTechnique/TestTechniqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CodeOnce.TestTechnique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTechniqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
